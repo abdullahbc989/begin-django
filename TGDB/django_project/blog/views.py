d4d0e98206324d3ba1de573a203c08ec570f1581
Abdullah Clute <clute.abdullahb@gmail.com>
2017-07-03 15:46:57 -0500

    Add files via upload

--- TGDB/django_project/blog/views.py
+++ TGDB/django_project/blog/views.py
@@ -8,39 +8,38 @@ from django.shortcuts import redirect, Http404, get_object_or_404, get_list_or_4
 def index(request):
     return HttpResponse("Hello Django")
 
-# view function to display a list of posts
 
+# view function to display a list of posts
 def post_list(request):
     posts = Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
 
-# view function to display a single post
 
-def post_detail(request, pk):
-    post = Post.objects.get(pk=pk)
+# view function to display a single post
+def post_detail(request, pk, post_slug):
+    post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
 
 
 # view function to display post by category
-
 def post_by_category(request, category_slug):
-    category = Category.objects.get(slug=category_slug)
-    posts = Post.objects.filter(category__slug=category_slug)
+    category = get_object_or_404(Category, slug=category_slug)
+    posts = get_list_or_404(Post, category=category)
     context = {
         'category': category,
         'posts': posts
     }
-    print(category)
     return render(request, 'blog/post_by_category.html', context)
 
 # view function to display post by tag
 
+
 def post_by_tag(request, tag_slug):
-    tag = Tag.objects.get(slug=tag_slug)
-    posts = Post.objects.filter(tags__name=tag)
+    tag = get_object_or_404(Tag, slug=tag_slug)
+    posts = get_list_or_404(Post, tags=tag)
     context = {
         'tag': tag,
-        'posts': posts
+        'posts': posts,
     }
     return render(request, 'blog/post_by_tag.html', context)
 
