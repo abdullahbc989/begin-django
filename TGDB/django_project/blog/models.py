1499c93876b8febb9add1a302d3ae2ab980adfc2
Abdullah Clute <clute.abdullahb@gmail.com>
2017-07-03 14:54:00 -0500

    Add files via upload

--- TGDB/django_project/blog/models.py
+++ TGDB/django_project/blog/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.template.defaultfilters import slugify
+from django.urls import reverse
 
 # Create your models here.
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Author Name")
+    email = models.EmailField(unique=True)
+    active = models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    last_logged_in = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('post_by_author', args=[self.name])
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(Author)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = "Categories"
+
+    def get_absolute_url(self):
+        return reverse('post_by_category', args=[self.slug])
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(Author)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('post_by_tag', args=[self.slug])
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, help_text="Slug will be generated automatically from the title of the post")
+    content = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category)
+    author = models.ForeignKey(Author)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Post, self).save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('post_detail', args=[self.id])
--- TGDB/django_project/blog/views.py
+++ TGDB/django_project/blog/views.py
@@ -1,14 +1,56 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.conf import settings
-import datetime
+from .models import Author, Tag, Category, Post
+from django.shortcuts import redirect, Http404, get_object_or_404, get_list_or_404
 
 
 def index(request):
     return HttpResponse("Hello Django")
 
+# view function to display a list of posts
 
-def today_is(request):
-    now = datetime.datetime.now()
-    return render(request, 'blog/datetime.html', {'now': now, "template_name": 'blog/nav.html',
-                                                  'base_dir': settings.BASE_DIR})
+def post_list(request):
+    posts = Post.objects.all()
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+# view function to display a single post
+
+def post_detail(request, pk):
+    post = Post.objects.get(pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+
+# view function to display post by category
+
+def post_by_category(request, category_slug):
+    category = Category.objects.get(slug=category_slug)
+    posts = Post.objects.filter(category__slug=category_slug)
+    context = {
+        'category': category,
+        'posts': posts
+    }
+    print(category)
+    return render(request, 'blog/post_by_category.html', context)
+
+# view function to display post by tag
+
+def post_by_tag(request, tag_slug):
+    tag = Tag.objects.get(slug=tag_slug)
+    posts = Post.objects.filter(tags__name=tag)
+    context = {
+        'tag': tag,
+        'posts': posts
+    }
+    return render(request, 'blog/post_by_tag.html', context)
+
+
+# view function to display post by author
+def post_by_author(request, name):
+    author = get_object_or_404(Author, name=name)
+    posts = get_list_or_404(Post, author=author)
+    context = {
+        'author': author,
+        'posts': posts
+    }
+    return render(request, 'blog/post_by_author.html', context )
